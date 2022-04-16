8a96095818f08b7a78c69d3e8e0122e4cc74f717
Falanke21 <frankhua21@gmail.com>
2022-04-16 18:26:44 -0400

    update run.py for powerpc

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env python3
 import subprocess
-import matplotlib.pyplot as plt
 from statistics import median
 
 if __name__ == "__main__":
@@ -25,30 +24,7 @@ if __name__ == "__main__":
         cpu_index = splited_lst.index("CPU")
         vector_times.append(float(splited_lst[cpu_index + 3]))
 
-    print("Running vector with custom log")
-    for i in range(count):
-        result = subprocess.run(
-            "./NPB3.0-omp-C/bin/vector-custom-log", stdout=subprocess.PIPE)
-        splited_lst = result.stdout.decode("utf-8").split()
-        cpu_index = splited_lst.index("CPU")
-        vector_log_times.append(float(splited_lst[cpu_index + 3]))
-
     median_scalar_t = median(scalar_times)
     median_vector_t = median(vector_times)
-    median_vector_log_t = median(vector_log_times)
 
-    # Plotting
-    x = ["Scalar", "Vector", "Vector with custom log"]
-    y = [median_scalar_t, median_vector_t, median_vector_log_t]
-    
-    fig = plt.figure()
-    
-    # creating the bar plot
-    plt.bar(x, y)
-
-    for index, data in enumerate(y):
-        plt.text(x=index, y =data*1.01, s=f"{data}", ha='center')
-
-    plt.ylabel("Time (s)")
-    plt.title("Gcc result for NASEP")
-    plt.savefig("result.png")
+    print(f"Scalar time: {median_scalar_t}, Vector time: {median_vector_t}")
